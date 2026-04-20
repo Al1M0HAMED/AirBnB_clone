cba0a737ee4b6c14c5a15aeaf336d096112efa0c
Ali Mohamed <alimohamed@192.168.1.11>
2026-04-21 00:47:06 +0200

    update __init__

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,8 @@ class BaseModel:
 	"""
 	base model class.
 	"""
-	def __init__(self, name=None, my_number= None, updated_at=None, id=None, created_at=None):
+	def __init__(self):
     
-		self.my_number = my_number
-		self.name = name
 		self.updated_at = datetime.now()
 		self.id = str(uuid.uuid4())
 		self.created_at = datetime.now()
@@ -33,12 +31,9 @@ class BaseModel:
 		"""
 		returns dict representaion.
 		"""
-		return {
-			"my_number": self.my_number,
-			"name": self.name,
-			"__class__": self.__class__.__name__,
-			"updated_at": self.updated_at.isoformat(),
-			"id": self.id,
-			"created_at": self.created_at.isoformat()
-		}
+		new_dict = self.__dict__.copy()
+		new_dict["__class__"] = self.__class__.__name__
+		new_dict["created_at"] = self.created_at.isoformat()
+		new_dict["updated_at"] = self.updated_at.isoformat()
+		return new_dict
 		
---
+++ t.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Doc
+"""
+from models.base_model import BaseModel
+
+
+class BaseModel(BaseModel):
+    """Doc
+    """
+
+    def __str__(self):
+        """Doc
+        """
+        return "Fake"
