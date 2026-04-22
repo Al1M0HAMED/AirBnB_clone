0711bd2d24d9c0e15eb8b810b18001dda6a9f1f7
Ali Mohamed <alimohamed@192.168.1.11>
2026-04-22 02:09:45 +0200

    filestorage objects is list of obj instead of list of dic

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,19 +14,37 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """
+        returns all objects.
+        """
         return (self.__objects)
 
     def new(self, obj):
-        obj_dict = obj.to_dict()
+        """
+        adds new obj to the list.
+        """
         key = str(obj.__class__.__name__) + "." + str(obj.id)
-        self.__objects[key] = obj_dict
+        self.__objects[key] = obj
 
     def save(self):
+        """
+        saves a json file from the obj list.
+        """
         if self.__objects is not None and len(self.__objects) >= 1:
+            tmp = {}
+        for k, obj in self.__objects.items():
+            tmp[k] = obj.to_dict()
             with open(self.__file_path, "w", encoding="utf-8") as file:
-                json.dump(self.__objects, file)
+                json.dump(tmp, file)
 
     def reload(self):
+        """
+        loads a list of objects from a json file.
+        """
+        from models.base_model import BaseModel
         if os.path.exists(self.__file_path):
             with open(self.__file_path, "r", encoding="utf-8") as file:
-                self.__objects = json.load(file)
+                data = json.load(file)
+
+            for key, value in data.items():
+                self.__objects[key] = BaseModel(**value)
