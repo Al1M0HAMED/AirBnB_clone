6bc584ef9989b2da3dc5498804eb776d8a2bbca4
Ali Mohamed <alimohamed@192.168.1.11>
2026-04-23 17:06:05 +0200

    file storage attributes unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,6 +18,11 @@ class TestFileStorage(unittest.TestCase):
         """
         test attributes.
         """
+    def test_FileStorage_file_path_is_private_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def testFileStorage_objects_is_private_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
         self.assertEqual(type(FileStorage()), FileStorage)
 
 
